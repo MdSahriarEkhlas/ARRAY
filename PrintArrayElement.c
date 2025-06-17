c932ccf2038cd11d3eb9ba072e4de76b7fb031f6
Md. Sahriar Ekhlas <251-15-104@diu.edu.bd>
2025-06-17 15:24:58 +0600

    Add files via upload

---
+++ PrintArrayElement.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    // Declaration & Initialization
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    // Print all elements of the array
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
